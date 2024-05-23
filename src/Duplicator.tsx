c189a1b7b03b52a6aa7ccdb81f71dbe1374a76c2
Muhammed Oguz <muhammed0748@outlook.com>
2024-05-23 23:14:59 +0300

    add initial project files for npm package

---
+++ src/Duplicator.tsx
@@ -0,0 +1,27 @@
+import { Fragment, type ReactNode } from 'react';
+
+type DuplicatorProps = {
+  /**
+   * Number of times to duplicate the children
+   */
+  count?: number;
+  /**
+   * The children to duplicate
+   */
+  children: ReactNode;
+};
+
+export const Duplicator = ({ count = 1, children, ...rest }: DuplicatorProps) => {
+  if (count < 1) {
+    console.error('Duplicator: count must be a positive integer');
+    return null;
+  }
+
+  return (
+    <Fragment {...rest}>
+      {[...Array(count)].map((_, index) => (
+        <Fragment key={index}>{children}</Fragment>
+      ))}
+    </Fragment>
+  );
+};
